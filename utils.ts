39270ec56eddcf7eaa3c5cd7229a2017592e417f
osigie <90554061+osigie@users.noreply.github.com>
2022-06-11 17:04:05 +0100

    created functions for compare and hashing

--- utils.ts
+++ utils.ts
@@ -1,9 +1,23 @@
-
-import jwt from "jsonwebtoken"
-
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
 const secret = process.env.JWT_SECRET as string;
 
-const generateToken = (id: string) => {
-  return jwt.sign({ id }, secret, { expiresIn: '30d' });
+export const generateToken = (id: string) => {
+  return jwt.sign({ id }, secret, { expiresIn: "30d" });
+};
+
+//Hash Password
+export const hashPassword = async (password: string) => {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  return hashedPassword;
+};
+
+ export const comparePassword = async (
+  oldPassword: string,
+  currentPassword: string
+) => {
+  const compare = await bcrypt.compare(oldPassword, currentPassword);
+  return compare;
 };
