de1312760a3ab2c0b6755cdd2fc081df666bcaaa
osigie <90554061+osigie@users.noreply.github.com>
2022-06-12 21:57:57 +0100

    done with user controller

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,10 +1,16 @@
-import userDb from "../models/users"
-import {NextFunction, Request, Response} from "express"
+import userDb from "../models/users";
+import { NextFunction, Request, Response } from "express";
 
-
-
-export const createUser = (req: Request, res: Response)=>{
-
-}
-
-
+export const createUser = async (req: Request, res: Response) => {
+  try {
+    const { name, street, postal, city } = req.body;
+    if (!name || !street || !postal || !city) {
+      res.status(400).json({ message: "please input all fields" });
+      return;
+    }
+    const user = await userDb.create({ name, street, city, postal });
+    res.status(201).json({ message: "user created" });
+  } catch (error) {
+    res.status(500).json({ message: "something went wrong" });
+  }
+};
